e6c3bfdf8e8379fa09269bc6c284964ab897351c
Aya Ting <ayateiiii@gmail.com>
2026-03-12 01:47:37 +0800

    fix: add site config for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ti-chu-website.pages.dev",
   output: "static",
   adapter: cloudflare({
     platformProxy: {
